2455dc55997825e7a626b24c3bc240b70ac29529
Luke Maxwell <24lbmaxwell@protonmail.com>
2025-12-20 23:14:03 -0500

    auto packaging

---
+++ template_projects/package_templates.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECTS=(kitbot_2025 skeleton)
+
+for project in "${PROJECTS[@]}"; do
+    rm -f $project.zip # Remove existing zip file if it exists
+
+    cd $project
+    zip -r ../$project.zip $(git ls-files .) > /dev/null
+    cd ..
+    echo "Generated $project.zip"
+done
