8d79035abdc45b781c5abd2e598b7be853d0d747
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-01-07 14:18:20 -0500

    fix project templates not including files with spaces

--- template_projects/package_templates.sh
+++ template_projects/package_templates.sh
@@ -2,11 +2,14 @@
 
 PROJECTS=(kitbot_2025 diff_drive skeleton)
 
+mkdir -p generated # Create generated directory if it doesn't exist
 for project in "${PROJECTS[@]}"; do
     rm -f $project.zip # Remove existing zip file if it exists
-
-    cd $project
-    zip -r ../$project.zip $(git ls-files .) > /dev/null
-    cd ..
+    rm -rf "generated/$project" # Clean up previous generated folder
+    mkdir -p "generated/$project"
+    git ls-files -z "$project" | xargs -0 -I {} cp --parents "{}" "generated"
+    cd "generated/$project"
+    zip -r ../$project.zip .
+    cd ../..
     echo "Generated $project.zip"
 done
