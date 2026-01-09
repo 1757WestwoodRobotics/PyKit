ae0183e3a75206dfe20c69bd57c18a34eef92053
Luke Maxwell <24lbmaxwell@protonmail.com>
2026-01-09 12:52:00 -0500

    remove unexposed values in 2026 robotpy hal

--- src/pykit/inputs/loggablesystemstats.py
+++ src/pykit/inputs/loggablesystemstats.py
@@ -1,8 +1,5 @@
 from hal import (
-    CAN_GetCANStatus,
     getBrownedOut,
-    getBrownoutVoltage,
-    getCPUTemp,
     getComments,
     getCommsDisableCount,
     getFPGAButton,
@@ -13,20 +10,6 @@ from hal import (
     getSystemActive,
     getSystemTimeValid,
     getTeamNumber,
-    getUserActive3V3,
-    getUserActive5V,
-    getUserActive6V,
-    getUserCurrent3V3,
-    getUserCurrent5V,
-    getUserCurrent6V,
-    getUserCurrentFaults3V3,
-    getUserCurrentFaults5V,
-    getUserCurrentFaults6V,
-    getUserVoltage3V3,
-    getUserVoltage5V,
-    getUserVoltage6V,
-    getVinCurrent,
-    getVinVoltage,
 )
 from ntcore import NetworkTableInstance
 from pykit.logtable import LogTable
@@ -49,44 +32,6 @@ class LoggedSystemStats:
         table.put("CommsDisabledCount", getCommsDisableCount()[0])
         table.put("RSLState", getRSLState()[0])
         table.put("SystemTimeValid", getSystemTimeValid()[0])
-
-        table.put("BatteryVoltage", getVinVoltage()[0])
-        table.put("BatteryCurrent", getVinCurrent()[0])
-
-        table.put("3v3Rail/Voltage", getUserVoltage3V3()[0])
-        table.put("3v3Rail/Current", getUserCurrent3V3()[0])
-        table.put("3v3Rail/Active", getUserActive3V3()[0])
-        table.put("3v3Rail/CurrentFaults", getUserCurrentFaults3V3()[0])
-
-        table.put("5vRail/Voltage", getUserVoltage5V()[0])
-        table.put("5vRail/Current", getUserCurrent5V()[0])
-        table.put("5vRail/Active", getUserActive5V()[0])
-        table.put("5vRail/CurrentFaults", getUserCurrentFaults5V()[0])
-
-        table.put("6vRail/Voltage", getUserVoltage6V()[0])
-        table.put("6vRail/Current", getUserCurrent6V()[0])
-        table.put("6vRail/Active", getUserActive6V()[0])
-        table.put("6vRail/CurrentFaults", getUserCurrentFaults6V()[0])
-
-        table.put("BrownoutVoltage", getBrownoutVoltage()[0])
-        table.put("CPUTempCelsius", getCPUTemp()[0])
-
-        canbusStatus = CAN_GetCANStatus()
-        (
-            percentBusUtilization,
-            busOffCount,
-            txFullCount,
-            receiveErrorCount,
-            transmitErrorCount,
-            errorStatus,
-        ) = canbusStatus
-
-        table.put("CANBus/PercentBusUtilization", percentBusUtilization)
-        table.put("CANBus/BusOffCount", busOffCount)
-        table.put("CANBus/TxFullCount", txFullCount)
-        table.put("CANBus/ReceiveErrorCount", receiveErrorCount)
-        table.put("CANBus/TransmitErrorCount", transmitErrorCount)
-        table.put("CANBus/ErrorStatus", errorStatus)
 
         ntClientsTable = table.getSubTable("NTClients")
 
